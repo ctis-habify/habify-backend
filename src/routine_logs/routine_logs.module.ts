8a5864d839754dab535eb4570daabd73ad5b356b
suedakca <akcasueda@gmail.com>
2025-12-05 15:17:58 +0300

    fix: routines logs and ctegories

--- src/routine_logs/routine_logs.module.ts
+++ src/routine_logs/routine_logs.module.ts
@@ -4,9 +4,10 @@ import { RoutineLogsService } from './routine_logs.service';
 import { RoutineLogsController } from './routine_logs.controller';
 import { RoutineLog } from './routine_logs.entity';
 import { Routine } from '../routines/routines.entity';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([RoutineLog, Routine])],
+  imports: [AuthModule, TypeOrmModule.forFeature([RoutineLog, Routine])],
   controllers: [RoutineLogsController],
   providers: [RoutineLogsService],
 })
