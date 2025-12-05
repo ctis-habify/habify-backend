39bb40ae0db428ae310a5b31e2c97de272e47cb6
kirciduru <kirciduru@gmail.com>
2025-12-05 09:21:52 +0300

    fix: added ApiProperty to routine list dto

--- src/routine_lists/dto/create-routine-list.dto.ts
+++ src/routine_lists/dto/create-routine-list.dto.ts
@@ -1,10 +1,19 @@
 import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger'; 
 
 export class CreateRoutineListDto {
+  @ApiProperty({
+    example: 'Sabah Rutinim',
+    description: 'Oluşturulacak rutin listesinin başlığı',
+  })
   @IsNotEmpty()
   @IsString()
   title: string;
 
+  @ApiProperty({
+    example: 1,
+    description: 'Bu listenin ait olduğu kategori IDsi (Int)',
+  })
   @IsNotEmpty()
   @IsNumber()
   categoryId: number;
